e999f095d6248d7ee6d7bd7d63e9b9da1f00d51c
Tyrin Holloway <tychnk@gmail.com>
2025-10-21 12:06:43 -0500

    finished chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { UUID } from 'crypto';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -113,4 +115,23 @@ export async function updateInvoice(
         throw new Error('Database Error: Failed to Delete Invoice.');
     }
     revalidatePath('/dashboard/invoices');
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
